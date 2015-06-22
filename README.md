fce3874ea2ca8b1a16c16d647052b7bf48d9526d
Jelly Joe <18262280912@163.com>
2015-06-22 18:06:33 +0800

    his->user_i

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ViewPageBackgroundChanged
 实现ViewPage背景颜色随着滑动渐变
+原理介绍：
+对于两个颜色Color1,Color2，每个颜色有三个主要颜色通道red通道,green通道,blue通道;
+如果这两个颜色作为前景色和背景色，前景色以透明度a显示，那么显示屏所得到的颜色按以下公式计算
+Color(最终颜色) = Color1（前景色） * a + Color2（背景色） * ( 1 - a )
+注意，在程序是现实，需要将三个颜色通道分开按公式计算
+
--- src/com/jellyjoe/viewpagerbgchange/MyActivity.java
+++ src/com/jellyjoe/viewpagerbgchange/MyActivity.java
@@ -1,15 +1,99 @@
 package com.jellyjoe.viewpagerbgchange;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+    private TextView mColor;
+    private ViewPager mViewPage;
+    private String[] mColors = {"#ff0000", "#00ff00", "#0000ff"};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initView();
+    }
+
+    private void initView() {
+        mColor = (TextView)findViewById(R.id.color);
+        mViewPage = (ViewPager)findViewById(R.id.viewpager);
+        View[] views = {View.inflate(this,R.layout.view_item,null), View.inflate(this,R.layout.view_item,null),
+                View.inflate(this,R.layout.view_item,null)};
+        mViewPage.setAdapter(new MyPageAdapter(views));
+        mViewPage.setBackgroundColor(Color.parseColor(mColors[0]));
+        mViewPage.setCurrentItem(0);
+
+        mViewPage.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int fontPosition, float offset, int i) {
+                if(fontPosition < 2 && fontPosition >= 0) {
+                    int nowColor = calculateColar(mColors[fontPosition], mColors[fontPosition + 1], offset);
+                    mViewPage.setBackgroundColor(nowColor);
+                    mColor.setBackgroundColor(nowColor);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int position) {
+
+            }
+        });
+    }
+
+    public class MyPageAdapter extends PagerAdapter {
+        private View[] mViews;
+        MyPageAdapter(View[] views) {
+            this.mViews = views;
+        }
+        @Override
+        public int getCount() {
+            return mViews.length;
+        }
+
+        public View[] getmViews() {
+            return mViews;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(mViews[position]);
+            return mViews[position];
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(mViews[position]);
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return false;
+        }
+    }
+
+    private int calculateColar(String color1, String color2, float offset) {
+        int fontColor = Color.parseColor(color1);
+        int laterColor = Color.parseColor(color2);
+        int ColorRed = (int)(Color.red(fontColor) * (1-offset) + Color.red(laterColor) * (offset));
+        int ColorGreen = (int)(Color.green(fontColor) * (1-offset) + Color.green(laterColor) * (offset ));
+        int ColorBlue = (int) (Color.blue(fontColor) * (1-offset) + Color.blue(laterColor) * (offset ));
+        mColor.setText("R:" + ColorRed + "---G:" + ColorGreen + "---B:" + ColorBlue);
+        return Color.rgb(ColorRed, ColorGreen, ColorBlue);
     }
 }
